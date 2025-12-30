9de9ec41a35640911abcac41ab2582ff24b1ef85
weter11 <sdf331@ukr.net>
2025-12-30 09:29:14 -0200

    Implement battery charge control checks
    Added battery control checks to the daemon.

--- daemon/src/main.rs
+++ daemon/src/main.rs
@@ -3,6 +3,8 @@ mod fan_daemon;
 mod hardware_control;
 mod hardware_detection;
 mod tuxedo_io;
+mod battery_control;
+
 use anyhow::Result;
 use tokio::signal;
 
@@ -15,6 +17,33 @@ async fn main() -> Result<()> {
     if unsafe { libc::geteuid() } != 0 {
         eprintln!("Error: Daemon must run as root");
         std::process::exit(1);
+    }
+
+    // Check hardware interface
+    if tuxedo_io::TuxedoIo::is_available() {
+        match tuxedo_io::TuxedoIo::new() {
+            Ok(io) => {
+                let interface = match io.get_interface() {
+                    tuxedo_io::HardwareInterface::Clevo => "Clevo",
+                    tuxedo_io::HardwareInterface::Uniwill => "Uniwill",
+                    tuxedo_io::HardwareInterface::None => "None",
+                };
+                log::info!("Detected hardware interface: {}", interface);
+                log::info!("Number of fans: {}", io.get_fan_count());
+            }
+            Err(e) => {
+                log::warn!("Failed to initialize tuxedo_io: {}", e);
+            }
+        }
+    } else {
+        log::warn!("/dev/tuxedo_io not available - some features will be disabled");
+    }
+
+    // Check battery charge control
+    if battery_control::BatteryControl::is_available() {
+        log::info!("Battery charge control (flexicharger) is available");
+    } else {
+        log::info!("Battery charge control not available");
     }
 
     // Start DBus service
